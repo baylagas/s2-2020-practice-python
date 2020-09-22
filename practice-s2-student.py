296f47be0899bfa2e64ae9a987a49b154e09b7f1
baylagas <balbino_a@hotmail.com>
2020-09-22 07:35:59 -0600

    init files

---
+++ practice-s2-student.py
@@ -0,0 +1,98 @@
+run = False
+"""
+-- practica 01 ------------------------------
+Write a Python program which accepts the radius of a circle from the user and compute the area.
+
+Escribir un programa python que acepte el radio de un circulo del usuario y pueda desplegar en
+consola el area de ese circulo.
+ejemplo: radio = 11, area = 380.13239
+
+"""
+
+
+"""
+-- practica 02 ------------------------------
+A car is traveling with a velocity of 10 m/s and it has a mass of 250 Kg. Compute its Kinetic energy?
+
+Si un auto esta viajando a una velocidad de 10 m/s y si su masa es 250 Kg. Encontrar su energia kinetica?
+
+"""
+
+
+"""
+-- practica 03 ------------------------------
+escriba un programa python que reciba el nombre y el apellido de un usuario pero al presentarlo
+en consola los presente apellido primero, un espacio vacio y luego el nombre
+ejemplo: name is aylagas balbino
+
+"""
+
+
+"""
+-- practica 04 ------------------------------
+escriba un programa python que reciba el nombre con extension de un archivo cualquiera del usuario
+y logre imprimir en consola solamente la extension sin el punto.
+ejemplo: hello.java -> java, practice.py -> py
+"""
+
+
+"""
+-- practica 05 ------------------------------
+escribir un programa python que reciba un numero entero n y calcule el valor de la operacion n+nn+nnn.
+ejemplo: ingresar 5 ->  resultado 615
+
+"""
+
+
+"""
+-- practica 06 ------------------------------
+escribir un programa python para imprimir el calendario de un mes y de un anio en especifico.
+importante: usar el modulo 'calendar' del core de python.
+
+"""
+
+
+"""
+-- practica 07 ------------------------------
+Write a Python program to get the volume of a sphere with radius 6.
+
+escribir un programa python que calcule el volumen de una espera con un radio obtenido desde el
+usaurio.
+ejemplo: radio 6 -> volumen
+
+"""
+
+
+"""
+-- practica 08 ------------------------------
+Escribe un programa python que muestre en pantalla el primer y el ultimo elemento de cualquier lista.
+por ejemplo: list = ["Red","Green","White" ,"Black"]
+print -> first: Red, last: Black
+
+Write a Python program to display the first and last colors from the any given list.
+example list = ["Red","Green","White" ,"Black"]
+print -> first: Red, last: Black
+
+"""
+
+
+"""
+-- practica 09 ------------------------------
+Write a Python program to get the difference between a given number and 17, if the number is
+greater than 17 return double the absolute difference.
+
+Escriba un programa python que obtenga del usuario un numero, con ese numero se obtendra la diferencia
+entre el numero y 17, si el numero es mayor que 17 se retornara el doble de la diferencia absoluta.
+ejemplo: number: 22, result->10 / number: 14, result->3
+
+"""
+
+"""
+-- practica 10 ------------------------------
+Write a Python program to calculate the sum of three given numbers, if the values are equal then return
+three times of their sum.
+
+escriba un programa python que calcule la suma de 3 numeros, si los valores de los numeros son iguales
+entonces se debe retornar 3 veces la suma
+
+"""
